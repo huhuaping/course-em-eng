cce161e70159e7b26dbd742a9768141e33bf71b2
huhuaping <huhuaping01@nwsuaf.edu.cn>
2025-02-19 18:16:42 +0800

    lecture 19 renew

--- lecture/code/code-chapter-19-identification.R
+++ lecture/code/code-chapter-19-identification.R
@@ -7,41 +7,44 @@ library(scales)
 
 # 19.1 Identification Problem====
 
-## example ====
-
 ### Scatter plots ====
 ## draw Hmisc::bezier graph and save objects
 source(here::here("lecture/code/Viz-draw-show.R"))
 
 # 19.3 Endogeneity test (Test of Simultaneity)====
 
-## Example ====
 
 ### First OLS estimation on price equation====
 
 load(here::here("data/truffles.rda"))
-names(truffles)  %<>% toupper()
+names(truffles) %<>% toupper()
 
-Hausman_models <-list(mod.P = P~PS+DI+PF,
-                      mod.Q.Hausman = Q~hat.Pi+hat.vi,
-                      mod.Q.Pindyck = Q~P+hat.vi)
+Hausman_models <- list(
+  mod.P = P ~ PS + DI + PF,
+  mod.Q.Hausman = Q ~ hat.Pi + hat.vi,
+  mod.Q.Pindyck = Q ~ P + hat.vi
+)
 
-P.red <- lm(Hausman_models$mod.P, data=truffles)
+P.red <- lm(Hausman_models$mod.P, data = truffles)
 
 truffles_Hausman <- truffles %>%
-  mutate(hat.Pi=round(P.red$fitted.values, digits = 4),
-         hat.vi=round(P.red$residuals, digits = 4))
+  mutate(
+    hat.Pi = round(P.red$fitted.values, digits = 4),
+    hat.vi = round(P.red$residuals, digits = 4)
+  )
 
 ### Second OLS estimation on Hausman equation====
 
-out_Hausman<-lm(Hausman_models$mod.Q.Hausman,
-                data =truffles_Hausman)
+out_Hausman <- lm(Hausman_models$mod.Q.Hausman,
+  data = truffles_Hausman
+)
 summary(out_Hausman)
 
 ### Second OLS estimation on Pindyck equation====
 
 out_Pindyck <- lm(Hausman_models$mod.Q.Pindyck,
-                data =truffles_Hausman)
+  data = truffles_Hausman
+)
 summary(out_Pindyck)
 
 ## check
@@ -52,7 +55,7 @@ instr <- ~ PS + DI + PF
 
 iv_fit <- systemfit::systemfit(
   formula = list(eq1, eq2), inst = instr,
-  method =  "2SLS", data = truffles
-  )
+  method = "2SLS", data = truffles
+)
 
-#hausman.systemfit()
+# hausman.systemfit()
